4b6fd78ec93a67b62c9c6f1610e17ea2eb5a0fd7
mohamedisakr <mohamedisakr@gmail.com>
2020-04-02 00:09:15 +0200

    finishing delete functionality

--- itemHistory.js
+++ itemHistory.js
@@ -4,7 +4,6 @@ import { pubsub } from "./pubsub.js";
 export default class ItemsHistory {
   constructor() {
     this.itemList = [];
-    // console.log(this);
   }
 
   render = container => {
@@ -16,6 +15,7 @@ export default class ItemsHistory {
       .addEventListener("click", this.handleClearAllItems);
     // add click event listener to list item
     const ul = displayItemsContainer.querySelector("ul");
+    ul.addEventListener("click", this.handleItemDeleted);
     console.log(ul);
 
     container.appendChild(displayItemsContainer);
@@ -41,6 +41,25 @@ export default class ItemsHistory {
     this.displayItems(this.itemList);
   };
 
+  handleItemDeleted = event => {
+    // get the current (clicked) item
+    const currentItem = event.target.closest("li");
+
+    // get the text
+    const val = currentItem.textContent;
+
+    // remove the removed element text from the array
+    this.itemList = this.itemList.filter(
+      item => item.toLowerCase() !== val.toLowerCase()
+    );
+
+    // remove the HTML element from the DOM
+    currentItem.parentElement.removeChild(currentItem);
+
+    // publish itemDeleted event
+    pubsub.publish("itemDeleted", this.itemList);
+  };
+
   displayItems = list => {
     const groceryList = document.querySelector(".grocery-list");
     groceryList.innerHTML = "";
@@ -50,7 +69,6 @@ export default class ItemsHistory {
     list.forEach(item => {
       const li = document.createElement("li");
       li.innerText = item;
-      // groceryList.appendChild(li);
       df.appendChild(li);
     });
     groceryList.appendChild(df);
